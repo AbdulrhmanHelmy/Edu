430472829a4c41095f005511efc0f0cdf61e9ab9
HelMy711 <abdulllrhmanhelmy@gmail.com>
2025-11-01 18:45:59 +0200

    initialize project with local DB

---
+++ src/test/java/com/edusystem/eduplatform/EduPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edusystem.eduplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
